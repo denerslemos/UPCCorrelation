45545f048cdd3cfc4b01ef6e2ac1a4f6588c4e13
denerslemos <denerslemos@gmail.com>
2025-05-09 22:57:30 +0200

    submission

--- HTCondor_submit_multiplicity.py
+++ HTCondor_submit_multiplicity.py
@@ -58,7 +58,7 @@ mkdir -p cond
 echo "Submit skim jobs at "
 echo PWD: $PWD
 
-root -l -b -q "correlation_XeXe.C(\\"$1\\", \\"$2\\", $3, $4, $5, $6, $7, $8, $9, ${10}, ${11}, ${12})"
+root -l -b -q "correlation_XeXe.C(\\"$1\\", \\"$2\\", $3, $4, $5, $6, $7, $8, $9, ${10})"
 """
 
 # Save to a shell script file
@@ -82,7 +82,7 @@ if(ratio == len(Lines)):
 log        = cond/'''+subFiles+'''.log
 output     = cond/'''+subFiles+'''.out
 error      = cond/'''+subFiles+'''.err
-arguments = '''+inFiles+'''.txt '''+str(outFiles)+''' 0 0 0 10 5 2.0 0 0 1 '''+str(uncerSys)+'''
+arguments = '''+inFiles+'''.txt '''+str(outFiles)+''' 0 0 10 5 1.0 1 0 '''+str(uncerSys)+'''
 queue
 '''
 
@@ -102,7 +102,7 @@ elif(ratio != len(Lines)):
 log        = cond/'''+subFiles+'''_part_'''+str(i)+'''.log
 output     = cond/'''+subFiles+'''_part_'''+str(i)+'''.out
 error      = cond/'''+subFiles+'''_part_'''+str(i)+'''.err
-arguments = '''+inFiles+'''_part'''+str(i)+'''.txt '''+str(outFiles)+'''_job_'''+str(i)+''' 0 0 0 10 5 2.0 0 0 1 '''+str(uncerSys)+'''
+arguments = '''+inFiles+'''_part'''+str(i)+'''.txt '''+str(outFiles)+'''_job_'''+str(i)+''' 0 0 10 5 1.0 1 0 '''+str(uncerSys)+'''
 queue
 '''
 		command_lines += temp
@@ -116,7 +116,7 @@ elif(ratio == 1):
 log        = cond/'''+subFiles+'''_part_'''+str(i)+'''.log
 output     = cond/'''+subFiles+'''_part_'''+str(i)+'''.out
 error      = cond/'''+subFiles+'''_part_'''+str(i)+'''.err
-arguments = '''+inFiles+'''_part'''+str(i)+'''.txt '''+str(outFiles)+'''_job_'''+str(i)+''' 0 0 0 10 5 2.0 0 0 1 '''+str(uncerSys)+'''
+arguments = '''+inFiles+'''_part'''+str(i)+'''.txt '''+str(outFiles)+'''_job_'''+str(i)+''' 0 0 10 5 1.0 1 0 '''+str(uncerSys)+'''
 queue
 '''
 		command_lines += temp
